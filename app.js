064c6a65d51edf76b4d22b6c2714f531b5bb11dc
Q. Price <priceqp@gmail.com>
2019-06-10 22:18:08 -0500

    Syntax Update and Clean Up

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //jshint esversion:6
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const _ = require("lodash");
-const app = express();
+const express = require("express"),
+      bodyParser = require("body-parser"),
+      mongoose = require("mongoose"),
+      _ = require("lodash"),
+      app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -19,29 +19,18 @@ mongoose.connect(process.env.DATABASEURL, {
 });
 
 // S C H E M A S
-const itemsSchema = {
-  name: String
-};
-const listSchema = {
-  name: String,
-  items: [itemsSchema]
-};
+const itemsSchema = {name: String},
+      listSchema = {name: String, items: [itemsSchema]};
 
 // M O D E L S
-const Item = mongoose.model("Item", itemsSchema);
-const List = mongoose.model("List", listSchema);
+const Item = mongoose.model("Item", itemsSchema),
+      List = mongoose.model("List", listSchema);
 
 // Default ToDos
-const item1 = new Item ({
-  name: "Welcome to your todolist!"
-});
-const item2 = new Item ({
-  name: "Hit the + button to add a new item."
-});
-const item3 = new Item ({
-  name: "<-- Hit this to delete an item."
-});
-const defaultItems = [item1, item2, item3];
+const item1 = new Item ({name: "Welcome to your todolist!"}),
+      item2 = new Item ({name: "Hit the + button to add a new item."}),
+      item3 = new Item ({name: "<-- Hit this to delete an item."}),
+      defaultItems = [item1, item2, item3];
 
 // R O U T E S
 app.get("/", function(req, res){
@@ -82,11 +71,9 @@ app.get("/:customListName", function(req,res){
 });
 
 app.post("/", function(req, res){
-  const itemName = req.body.newItem;
-  const listName = req.body.list;
-  const item = new Item ({
-    name: itemName
-  });
+  const itemName = req.body.newItem,
+        listName = req.body.list,
+        item = new Item ({name: itemName});
   if(listName === "Today"){
     item.save();
     res.redirect("/");
@@ -100,8 +87,8 @@ app.post("/", function(req, res){
 });
 
 app.post("/delete", function(req, res){
-  const checkedItemId = req.body.checkbox;
-  const listName = req.body.listName;
+  const checkedItemId = req.body.checkbox,
+        listName = req.body.listName;
   if(listName === "Today"){
     Item.findByIdAndRemove(checkedItemId, function(err){
       if(!err){
